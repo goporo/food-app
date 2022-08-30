fd3bfda0bd114a755d9c78f58429488fcf9f774b
goporo <goporolo1@gmail.com>
2022-08-30 19:07:24 +0700

    home banner done, start doing responsive

--- src/components/Home/HomeBanner.jsx
+++ src/components/Home/HomeBanner.jsx
@@ -2,17 +2,24 @@ import React from 'react'
 import images from 'assets/imgs'
 import './HomeBanner.scss'
 
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
 const HomeBanner = () => {
     return (
-        <div className='home-banner' style={{ backgroundImage: images.home_banner }}>
+        <div className='home-banner' style={{ backgroundImage: `url(${images.home_banner})` }}>
             <div className="home-banner__container">
                 <div className='home-banner__wrapper'>
                     <div className='home-banner__title'>ENJOY YOUR MEAL</div>
                     <div className='home-banner__desc'>
-                        Good food is wise
+                        Good food is wise <br />
                         <strong>medicine</strong>
                     </div>
-                    <div className='home-banner__button'>ORDER NOW</div>
+                    <div className='home-banner__button'>
+                        <span>
+                            <AddShoppingCartIcon />
+                            ORDER NOW
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
